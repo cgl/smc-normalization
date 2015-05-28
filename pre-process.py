7fde7b3d338d389b38068eeda13b9ac635a3934e
Çağıl Uluşahin <cagilulusahin@gmail.com>
2015-05-28 21:13:53 +0300

    pre-process added

---
+++ pre-process.py
@@ -0,0 +1,11 @@
+import langid
+
+def read_file_direct(self, infile, lang='en'):
+    with open(infile) as tweet_file:
+        W = None
+        for line in tweet_file:
+            if line.split('\t')[0] == 'W':
+                W = line.split('\t')[1].strip('\n').decode('utf-8')
+                if not (W is None) | (W == 'No Post Title'):
+                    if langid.classify(W)[0] == lang:
+                        yield W
